45e583880e6240c2033f8e4d764bb75e63f8a6f3
bmedeiros1987 <bmedeiros1987@gmail.com>
2026-09-06 07:00:28 -0300

    feat: cache Universal Import Center for offline use

--- app/www/service-worker.js
+++ app/www/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = 'voyage-shell-v1';
-const CORE = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest'];
+const CACHE = 'voyage-shell-v2';
+const CORE = ['./', './index.html', './styles.css', './themes.css', './imports.css', './app.js', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(CORE)));
